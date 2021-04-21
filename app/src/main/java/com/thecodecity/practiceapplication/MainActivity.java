c13883eee824805e7dd4b7f6c58dda8ef155ff76
archita2511 <archita.ekkirala@gmail.com>
2021-04-21 14:43:03 +0530

    Current location feature added in Registration page

--- app/src/main/java/com/thecodecity/practiceapplication/MainActivity.java
+++ app/src/main/java/com/thecodecity/practiceapplication/MainActivity.java
@@ -1,8 +1,5 @@
 package com.thecodecity.practiceapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +9,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
--- app/src/main/java/com/thecodecity/practiceapplication/SignUpActivity.java
+++ app/src/main/java/com/thecodecity/practiceapplication/SignUpActivity.java
@@ -1,21 +1,31 @@
 package com.thecodecity.practiceapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,6 +40,14 @@ public class SignUpActivity extends AppCompatActivity {
    private RadioButton customerButton, retailorButton, wholesalorButton;
    private Button Register;
    private ProgressDialog loadingBar;
+private Double lati, longi;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+    Context context = this;
+    Button fetch;
+ //   TextView user_location;
+    private FusedLocationProviderClient mFusedLocationClient;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +63,106 @@ public class SignUpActivity extends AppCompatActivity {
         wholesalorButton = (RadioButton) findViewById(R.id.WholesalerButton);
         txtphonenumber = (EditText) findViewById(R.id.PhoneNumberEditText);
         loadingBar = new ProgressDialog(this);
+
+
+        fetch = findViewById(R.id.fetch_location);
+      //  user_location = findViewById(R.id.user_location);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                fetchLocation();
+
+
+            }
+        });
+    }
+
+    private void fetchLocation() {
+
+        if (ContextCompat.checkSelfPermission(SignUpActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(SignUpActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                new AlertDialog.Builder(this)
+                        .setTitle("Required Location Permission")
+                        .setMessage("You have to give this permission to acess this feature")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(SignUpActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(SignUpActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+
+            // Permission has already been granted
+            mFusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            Toast.makeText(SignUpActivity.this,"goingg "+location,Toast.LENGTH_SHORT).show();
+
+                            if (location != null) {
+
+                                // Logic to handle location object
+                                Double latittude = location.getLatitude();
+                                Double longitude = location.getLongitude();
+
+                           //     user_location.setText("Latitude = "+latittude + "\nLongitude = " + longitude);
+                                lati=latittude;
+                                longi=longitude;
+
+                            }
+                        }
+                    });
+
+        }
+
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //abc
+            }else{
+
+            }
+        }
     }
 
     public void createAccount(View view) {
@@ -106,6 +224,8 @@ public class SignUpActivity extends AppCompatActivity {
                     userdataMap.put("password", password);
                     userdataMap.put("name", name);
                     userdataMap.put("email",email);
+                    userdataMap.put("latitude",lati);
+                    userdataMap.put("longitude",longi);
                     RootRef.child("Users").child(name).updateChildren(userdataMap)
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
                                 @Override
